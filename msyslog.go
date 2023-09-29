03b67a7b4c0c00bf17f239c66f1931ceda0247ba
William John Holden <wjholden@gmail.com>
2023-09-29 14:06:18 +0200

    Initial

---
+++ msyslog.go
@@ -0,0 +1,63 @@
+package msyslog
+
+import (
+	"bytes"
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+type Severity uint8
+
+const (
+	EMERGENCY     Severity = 0
+	ALERT         Severity = 1
+	CRITICAL      Severity = 2
+	ERROR         Severity = 3
+	WARNING       Severity = 4
+	NOTICE        Severity = 5
+	INFORMATIONAL Severity = 6
+	DEBUG         Severity = 7
+)
+
+type MSyslog struct {
+	Facility uint8
+	Severity Severity
+	Hostname string
+	AppName  string
+	ProcId   string
+	MsgId    string
+	conn     *net.UDPConn
+}
+
+func (m MSyslog) Write(p []byte) (int, error) {
+	level := m.Facility*8 + uint8(m.Severity)
+	version := 1
+	timestamp := time.Now().Format(time.RFC3339)
+	var buf bytes.Buffer
+	fmt.Fprintf(&buf, "<%d>%d %s %s %s %d ", level, version, timestamp, m.Hostname, m.AppName, os.Getpid())
+	buf.Write(p)
+	return m.conn.Write(buf.Bytes())
+}
+
+func New(address *net.IP, port uint16) (MSyslog, error) {
+	var logger MSyslog
+	dst := net.UDPAddr{IP: *address, Port: int(port)}
+	conn, err := net.DialUDP("udp", nil, &dst)
+	if err != nil {
+		return logger, err
+	}
+	logger.conn = conn
+	logger.Facility = 23
+	logger.Severity = DEBUG
+	logger.Hostname, _ = os.Hostname()
+	logger.AppName = "-"
+	logger.ProcId = "-"
+	return logger, nil
+}
+
+func (m MSyslog) Close() error {
+
+	return m.conn.Close()
+}
---
+++ msyslog_test.go
@@ -0,0 +1,18 @@
+package msyslog
+
+import (
+	"fmt"
+	"net"
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	addr := net.ParseIP("ff02::514")
+	var port uint16 = 514
+	mlogger, err := New(&addr, port)
+	if err != nil {
+		t.Error(err)
+	}
+	defer mlogger.Close()
+	fmt.Fprint(mlogger, "This is a test message.")
+}
